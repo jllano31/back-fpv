09b0f401da26c1e558e74400620cc8a129a25b3d
Juan Sebastián Llano <jllano@cidenet.com.co>
2024-10-24 00:16:06 -0500

    :tada: primer commit

---
+++ backFpv/src/main/java/backFpv/BackFpvApplication.java
@@ -0,0 +1,13 @@
+package backFpv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackFpvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackFpvApplication.class, args);
+	}
+
+}
---
+++ backFpv/src/test/java/backFpv/BackFpvApplicationTests.java
@@ -0,0 +1,13 @@
+package backFpv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackFpvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
